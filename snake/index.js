1bad948c3b187a4320221cfe11c81c77a18da9ae
frankensteigner <frankensteigner@gmail.com>
2020-11-22 05:18:27 +0000

    save all

--- snake/index.js
+++ snake/index.js
@@ -2,13 +2,16 @@
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
 
-// in theory will run program again when play again button pressed. doesn't work yet! 
+// in theory this will run program again when play again button pressed. doesn't work yet! 
 // button.addEventListener("click", runProgram);
   
 function runProgram(){
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////// SETUP /////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+
+                /* //////////////////////////////////////////////////////////////////////////
+                                            SETUP
+                                initial var declarations go here!
+                                    snake array goes here!
+                ////////////////////////////////////////////////////////////////////////// */ 
 
   // Constant Variables
   var FRAMES_PER_SECOND_INTERVAL = 100;
@@ -31,6 +34,7 @@ function runProgram(){
   var BOARD_SIZE = $("#board").width();   // the height and width are equal
   var SQUARE_SIZE = $("#apple").width();  // the size of the apple is the same size as all squares
 
+  // upon eating the apple a new object is added to the array ()
   var snake = [
       {positionX: 20, positionY: 20}, // head
     ]
@@ -50,31 +54,144 @@ function runProgram(){
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
-  ////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+                /* //////////////////////////////////////////////////////////////////////////
+                                            CORE LOGIC
+                                All functions called in newFrame go here!
+                ////////////////////////////////////////////////////////////////////////// */ 
 
-  /* 
-  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-  by calling this function and executing the code inside.
-  */
+  // called every interval second (see var interval)
   function newFrame() {
     // repositionGameItem();
     // redrawGameItem();
     testRepositionGameItem();
     testRedrawGameItem();
-    keepScreen();
+    keepScreen(); 
     eat()
-    checkEndgame();
+    checkEndgame(); // comment this function out to remove endgame and have snake bounce off walls
   }
 
   newFrame();
 
-  /* 
-  Called in response to arrow keys being pressesd
-  */
+    // repositions the head of the snake
 
-  function handleKeyDown(event) {
+    function repositionGameItem() {
+            snake[0].positionX += velocityX; // update the position of the box on x axis 
+            snake[0].positionY += velocityY; // update the position of the box on the y axis
+    }
+
+    // test function that will reposition the head AND the body pieces
+
+    function testRepositionGameItem() {
+        if (snake.length === 1) {
+            snake[0].positionX += velocityX; // update the position of the head on x axis 
+            snake[0].positionY += velocityY; // update the position of the head on the y axis
+        } else if (snake.length > 1) {
+            snake[0].positionX += velocityX; // update the position of the head on x axis 
+            snake[0].positionY += velocityY; // update the position of the head on the y axis
+            // for loop will update the position of the body to be square in front of it, counting down to index 0 from the tail
+            for (var i = (snake.length - 1); i > 1; i--) {
+                console.log("updated indexed positions");
+                snake[i].positionX = snake[i-1].positionX;
+                snake[i].positionY = snake[i-1].positionY;
+            }
+        }
+    }
+    
+    // redraws the head of the snake
+
+    function redrawGameItem() {
+        $("#snake").css("left", snake[0].positionX); // draws the head positionX pixels away from 'left' location
+        $("#snake").css("top", snake[0].positionY); // draws the head positionY pixels away from 'top' location
+    }
+
+    // test function that will redraw the head AND the body pieces
+
+    function testRedrawGameItem() {
+        if (snake.length === 1) {
+            $("#snake").css("left", snake[0].positionX); // draws the head positionX pixels away from 'left' location
+            $("#snake").css("top", snake[0].positionY); // draws the head positionY pixels away from 'top' location
+        } else if (snake.length > 1) {
+            $("#snake").css("left", snake[0].positionX); // draws the head positionX pixels away from 'left' location
+            $("#snake").css("top", snake[0].positionY); // draws the head positionY pixels away from 'top' location
+            // for loop will redraw the body piece to be where the piece in front of it is, counting down from the tail
+            for (var i = (snake.length - 1); i > 0; i--) {
+                $("#snake" + snake.length).css("left", snake[i - 1].positionX); 
+                $("#snake" + snake.length).css("top", snake[i - 1].positionY);
+            }
+            console.log("redrew body pieces");
+        }
+    }
+
+
+// checks for collision w/ apple, changes apple position and adds body piece if true
+
+  function eat() {
+    if (doCollide(apple, snake[0]) === true) {
+        moveApple();
+        addSnake();
+        points = (points + 1);
+        $('.score').text("SCORE: " + points);
+        console.log("eat function worked");
+    } else {
+        console.log("eat function did NOT work");
+    }
+  }
+
+  // Keeps the snake in the left and right borders of the box 
+  // this should probably be a helped function but I still call it in newFrame so I'm putting it here
+
+    function keepScreen() {
+                if (snake[0].positionX > 400) {
+                    velocityX = -velocityX;
+				} else if (snake[0].positionX < 20) {
+                    velocityX = -velocityX;
+                } else if (snake[0].positionY > 400) {
+                    velocityY = -velocityY;
+			    } else if (snake[0].positionY < 20) {
+                    velocityY = -velocityY;
+				} 
+            }
+
+    // // checks for collisions with the walls and snake body, and runs endgame if true
+    function checkEndgame() {
+        if (snake[0].positionX > 400) {
+            endgame()
+			} else if (snake[0].positionX < 20) {
+            endgame()
+            } else if (snake[0].positionY > 400) {
+            endgame()
+			} else if (snake[0].positionY < 20) {
+            endgame()
+            } 
+            else { for (var i = (snake.length - 2); i > 1; i--) {
+            if (doCollide(snake[0], snake[i])) {
+                endgame();
+            } }
+        }
+    }
+
+    // display endgame screen and stop interval timer
+    // this should also be in helper functions but I got tired of scrolling down to find it so it's here
+    function endgame() {
+        // displays the game over css
+        $("#gameover").css({opacity: 0.7});
+        // removes the score from below the screen
+        $("#gamescore").css({opacity: 0});
+        // stop the interval timer
+        clearInterval(interval);
+        // turn off event handlers
+        $(document).off();
+    }
+
+
+                /* //////////////////////////////////////////////////////////////////////////
+                                            HELPER FUNCTIONS
+                                All functions NOT called in newFrame go here!
+                ////////////////////////////////////////////////////////////////////////// */ 
+
+
+  // updates velocity when arrow keys pressed. updated velocity used in the resposition function to move head
+    function handleKeyDown(event) {
       if (event.which === KEY.LEFT) {
           console.log("left arrow key pressed");
             velocityX = -20;
@@ -94,35 +211,14 @@ function runProgram(){
       }
   }
 
-// checks for collision w/ apple, changes apple position and adds body piece if true
 
-  function eat() {
-    if (doCollide(apple, snake[0]) === true) {
-        moveApple();
-        addSnake();
-        points = (points + 1);
-        $('.score').text("SCORE: " + points);
-        console.log("eat function worked");
-    } else {
-        console.log("eat function did NOT work");
+    // generates the random integer in moveApple
+    function randomInteger(max) {
+            var randomInt = Math.floor(Math.random() * max);
+            return randomInt;
     }
-  }
 
-  // Keeps the snake in the left and right borders of the box 
-
-    function keepScreen() {
-                if (snake[0].positionX > 400) {
-                    velocityX = -velocityX;
-				} else if (snake[0].positionX < 20) {
-                    velocityX = -velocityX;
-                } else if (snake[0].positionY > 400) {
-                    velocityY = -velocityY;
-			    } else if (snake[0].positionY < 20) {
-                    velocityY = -velocityY;
-				} 
-            }
-
-    // updates position of apple when eaten
+    // updates position of apple when 'eaten' 
 
     function moveApple() {
         apple.positionX = randomInteger(BOARD_SIZE/SQUARE_SIZE) * 20; // generates a random new x coordinate for the apple
@@ -141,6 +237,7 @@ function runProgram(){
             }    
         }   
     }
+
 
     // Adds body piece to the snake
 
@@ -167,99 +264,16 @@ function runProgram(){
         snake.push(newBody);
     }
 
-    // 
-
-    // checks for collisions with the walls and snake body, and runs endgame if true
-    function checkEndgame() {
-        if (keepScreen === true) {
-            endgame()
-        } 
-        for (var i = 1; i < snake.length; i++) {
-            if (doCollide(snake[0], snake[i])) {
-                endgame();
-            }
-        }
-    }
-
-    // display endgame screen and stop interval timer
-    function endgame() {
-        document.getElementById("endgame").style.opacity = "0.7";
-        document.getElementById("gamescore").style.opacity = "0.0";
-        clearInterval(interval);
-    }
-    
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
-
-    // do collide
+    // checks for collisions 
 
     function doCollide(obj1, obj2) {
         // if position x and position y of object 1 and 2 are the same, then there is a collision
         if (obj1.positionX === obj2.positionX && obj1.positionY === obj2.positionY) { 
-        console.log("collision");
+        console.log("collision"); // THIS IS BEING LOGGED AND I DON'T KNOW WHY? game still works though
         return true;
             } else {
              return false;
             }
         }
-
-    function randomInteger(max) {
-        var randomInt = Math.floor(Math.random() * max);
-        return randomInt;
-    }
-
-    function repositionGameItem() {
-            snake[0].positionX += velocityX; // update the position of the box on x axis 
-            snake[0].positionY += velocityY; // update the position of the box on the y axis
-    }
-
-    // test function that will reposition the body pieces and the head
-
-    function testRepositionGameItem() {
-        if (snake.length === 1) {
-            snake[0].positionX += velocityX; // update the position of the head on x axis 
-            snake[0].positionY += velocityY; // update the position of the head on the y axis
-        } else if (snake.length > 1) {
-            snake[0].positionX += velocityX; // update the position of the head on x axis 
-            snake[0].positionY += velocityY; // update the position of the head on the y axis
-            // for loop will update the position of the body to be square in front of it, counting down to index 0 from the tail
-            for (var i = (snake.length - 1); i > 0; i--) {
-                console.log("updated indexed positions");
-                snake[i].positionX = snake[i-1].positionX;
-                snake[i].positionY = snake[i-1].positionY;
-            }
-        }
-    }
-
-    function redrawGameItem() {
-        $("#snake").css("left", snake[0].positionX); // draws the head positionX pixels away from 'left' location
-        $("#snake").css("top", snake[0].positionY); // draws the head positionY pixels away from 'top' location
-    }
-
-    // test function that will redraw the body pieces and the head
-
-    function testRedrawGameItem() {
-        if (snake.length === 1) {
-            $("#snake").css("left", snake[0].positionX); // draws the head positionX pixels away from 'left' location
-            $("#snake").css("top", snake[0].positionY); // draws the head positionY pixels away from 'top' location
-        } else if (snake.length > 1) {
-            $("#snake").css("left", snake[0].positionX); // draws the head positionX pixels away from 'left' location
-            $("#snake").css("top", snake[0].positionY); // draws the head positionY pixels away from 'top' location
-            // for loop will redraw the body piece to be where the piece in front of it is, counting down to index 0 from the tail
-            for (var i = (snake.length - 1); i > 0; i--) {
-                $("#snake" + snake.length).css("left", snake[i].positionX); 
-                $("#snake" + snake.length).css("top", snake[i].positionY);
-            }
-        }
-    }
   
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
-
-    // turn off event handlers
-    $(document).off();
-  }
 }
